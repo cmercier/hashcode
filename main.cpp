90b6e97bbe12c7994f1748c78501d687a944cb91
cmercier <clemmercier@hotmail.com>
2015-03-12 19:47:42 +0100

    ouverture fichier

--- main.cpp
+++ main.cpp
@@ -1,9 +1,20 @@
 #include <iostream>
+#include <fstream>
 
 using namespace std;
 
 int main()
 {
+    ifstream fichier("dc.txt", ios::in);  // on ouvre le fichier en lecture
+
+    if(fichier)  // si l'ouverture a réussi
+    {
+        // instructions
+            fichier.close();  // on ferme le fichier
+    }
+    else  // sinon
+            cerr << "Impossible d'ouvrir le fichier !" << endl;
+
     cout << "Hello World!" << endl;
     return 0;
 }
