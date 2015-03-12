654734dc64996ff9da1df318550ddc48dadeea2f
Thomas Riccioli <thomas.riccioli@etu.utc.fr>
2015-03-12 19:53:54 +0100

    added structures for the data models

--- main.cpp
+++ main.cpp
@@ -1,6 +1,24 @@
 #include <iostream>
+#include <vector>
 
 using namespace std;
+
+int R, // Nb de rangees du centre
+    S, // Nb emplacements de chaque rangee
+    U, // Nb emplacements indisponibles
+    P, // Nb de groupes a creer
+    M; // Nb de serveurs a allouer
+
+typedef struct emplacement {
+    int r, c; // rangee & colonne
+} Emplacement;
+
+typedef struct serveur {
+    int t, c; // taille & capacite
+} Serveur;
+
+vector<Emplacement> emplIndisponibles;
+vector<Serveur> serveurAAllouer;
 
 int main()
 {
