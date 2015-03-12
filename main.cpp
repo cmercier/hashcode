5dba995ea88169dc3684b394d7010b06d3f37251
Thomas Riccioli <thomas.riccioli@etu.utc.fr>
2015-03-12 21:31:25 +0100

    Refactoring

--- main.cpp
+++ main.cpp
@@ -27,7 +27,55 @@ vector<Serveur*> serveurAAllouer;
 
 bool grille[MAX_INT][MAX_INT];
 
-void init () {
+bool read_input_file (string filename) {
+    FILE *f = NULL;
+    f = fopen(filename, "r+");
+    bool success = false;
+    if (f) {
+        success = true;
+        fscanf(f, "%d %d %d %d %d\n", &R, &S, &U, &P, &M);
+        for (int i = 0; i < U; i++) {
+            Emplacement *e = new Emplacement();
+            fscanf(f, "%d %d\n", &(e->r), &(e->c));
+            emplIndisponibles.push_back(e);
+
+            grille[e->r][e->c] = true;
+
+            //cout << e->r << "-" << e->c << endl;
+        }
+        for (int i = 0; i < U; i++) {
+            Serveur *s = new Serveur();
+            fscanf(f, "%d %d\n", &(s->t), &(s->c));
+            serveurAAllouer.push_back(s);
+
+            //cout << s->t << "-" << s->c << endl;
+        }
+        fclose(f);
+    }
+    return success;
+}
+
+bool print_output_file (string filename) {
+    FILE *f = NULL;
+    f = fopen(filename, "w+");
+    bool success = false;
+    if (f) {
+        success = true;
+        for (int i = 0; i < serveurAAllouer.size(); i++) {
+            Serveur *s = serveurAAllouer[i];
+            if (1) {
+                fprintf(outputFile, "%d %d %d\n", 0, 0, 0);
+            } else {
+                fprintf(outputFile, "x\n");
+            }
+        }
+        fclose(f);
+    }
+    return success;
+}
+
+void init_vars () {
+    read_input_file("dc.in");
     for (int i = 0; i < R; i++) {
         for (int j = 0; j < S; j++) {
             grille[i][j] = false;
@@ -42,6 +90,9 @@ bool triPerformance(Serveur* i, Serveur* j)
 
 int main()
 {
+    init_vars();
+    print_output_file("dc.txt");
+
     FILE *f = NULL;
     f = fopen("dc.in", "r+");
 
